4488155d87f1afae3ffa50fea50d063d3656402f
MohdZibran <2k22.it.32328@gmail.com>
2024-11-23 00:41:19 +0530

    Time: 11 ms (34.31%), Space: 14.5 MB (5.05%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -3,54 +3,25 @@ public:
     int romanToInt(string s) {
         int sum=0;
         int n=s.size();
-        for(int i=0;i<n;i++)
-        {
-         if(s[i]=='I'&&s[i+1]=='V') {
-            sum+=4;
-            i+=2;
+        map<char,int>mp;
+        mp.insert({'I',1});
+        mp.insert({'V',5});
+        mp.insert({'X',10});
+        mp.insert({'L',50});
+        mp.insert({'C',100});
+        mp.insert({'D',500});
+        mp.insert({'M',1000});
+    
+    for(int i=n-1;i>=0;i--){
+        if(mp[s[i]]<mp[s[i+1]]){
+            sum=sum-(mp[s[i]]);
         }
-         else if(s[i]=='I'&&s[i+1]=='X'){
-            sum+=9;
-            i+=1;
+        else {
+            sum+=mp[s[i]];
         }
-        else if(s[i]=='X'&&s[i+1]=='C'){
-            sum+=90;
-            i+=1;
-        }
-        else if(s[i]=='C'&&s[i+1]=='D'){
-            sum+=400;
-            i+=1;
-        }
-        else if(s[i]=='X'&&s[i+1]=='L'){
-            sum+=40;
-            i+=1;
-        }
-        else if(s[i]=='C'&&s[i+1]=='M'){
-            sum+=900;
-            i+=1;
-        }
-        else if(s[i]=='V'){
-        sum+=5;
-        }
-        else if(s[i]=='X'){
-        sum+=10;
-        }
-        else if(s[i]=='L'){
-        sum+=50;
-        }
-        else if(s[i]=='C'){
-        sum+=100;
-        }
-        else if(s[i]=='D'){
-        sum+=500;
-        }
-        else if(s[i]=='M'){
-        sum+=1000;
-        }
-        else if(s[i]=='I'){
-           sum+=1;
-        } 
     }
-    return sum;
-    }  
+
+        return sum;
+    
+    }
 };
