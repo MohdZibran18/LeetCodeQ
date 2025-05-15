5f066dd53b4256598f4ef5ba286b662706ef72de
MohdZibran <2k22.it.32328@gmail.com>
2025-05-15 15:38:11 +0530

    Time: 28 ms (17.63%), Space: 30 MB (55.31%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+    map<int, int> ans;
+    map<int, pair<int, int>> mp;
+    int maxFreq = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        ans[nums[i]]++;
+        if (mp.find(nums[i]) == mp.end()) {
+            mp[nums[i]].first = i;
+        }
+        mp[nums[i]].second = i;
+        maxFreq = max(maxFreq, ans[nums[i]]);
+    }
+
+    int minLen = nums.size();
+    for (auto it : ans) {
+        if (it.second == maxFreq) {
+            int len = mp[it.first].second - mp[it.first].first + 1;
+            minLen = min(minLen, len);
+        }
+    }
+    return minLen;
+}
+
+};
