10f98d53c8fecf707f5fbc99f91769a6990633bf
MohdZibran18 <2k22.it.32328@gmail.com>
2025-07-10 14:37:41 +0530

    Time: 0 ms (100%), Space: 50 MB (91.06%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,10 +1,38 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        //brute force is using xor 
-        int ans=0;
-        for(int i=0;i<nums.length;i++){
-            ans^=nums[i];
+  public int singleNonDuplicate(int[] nums) {
+    int low = 0, high = nums.length - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        // Handle edge cases first
+        if (mid == 0 || mid == nums.length - 1) {
+            return nums[mid];
         }
-        return ans;
+
+        // Check if mid is the unique element
+        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+            return nums[mid];
+        }
+        if (nums[mid] == nums[mid - 1]) {
+            int leftCount = mid - low + 1;
+            if (leftCount % 2 == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 2;
+            }
+        }
+        else if (nums[mid] == nums[mid + 1]) {
+            int rightCount = high - mid + 1;
+            if (rightCount % 2 == 0) {
+                high = mid - 1;
+            } else {
+                low = mid + 2;
+            }
+        }
     }
+    return -1;
+}
+
+
 }
