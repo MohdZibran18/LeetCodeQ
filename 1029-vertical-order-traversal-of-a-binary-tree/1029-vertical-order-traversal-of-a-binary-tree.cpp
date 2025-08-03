b4d6503cbe17041c3d9debc489ddb036f9cab440
Mohd Zibran <2k22.it.32328@gmail.com>
2025-08-03 23:02:52 +0530

    Time: 0 ms (100%), Space: 16.5 MB (30.62%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,32 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> nodes;
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        todo.push({root, {0, 0}});
-        while (!todo.empty()) {
-            auto p = todo.front();
-            todo.pop();
-            TreeNode* node = p.first;
-            int ver = p.second.first, lvl = p.second.second;
-            nodes[ver][lvl].insert(node->val);
-            if (node->left) {
-                todo.push({node->left, {ver - 1, lvl + 1}});
+    //  V     lev     multiset which will sort the data 
+    map<int,map<int,multiset<int>>>mp;
+                //node        ver   lev
+    queue<pair<TreeNode*,pair<int,int>>>que;
+    que.push({root,{0,0}});
+    while(!que.empty()){
+        int size= que.size();
+        for(int i=0;i<size;i++){
+            auto ptr =que.front();
+            TreeNode* node =ptr.first;
+            int lvl=  ptr.second.second;
+            int ver= ptr.second.first;
+            que.pop();
+            mp[ver][lvl].insert(node->val);
+            if(node->left){
+                que.push({node->left,{ver-1,lvl+1}});
             }
-            if (node->right) {
-                todo.push({node->right, {ver + 1, lvl + 1}});
+            if(node->right){
+                que.push({node->right,{ver+1,lvl+1}});
+            } 
+        }
+    }
+    vector<vector<int>>ans1;
+    for(auto it :mp ){
+        vector<int>ans;
+        for(auto it1 : it.second){
+            for(auto it2 : it1.second ){
+                ans.push_back(it2);
             }
         }
-        vector<vector<int>> ans;
-        for (auto it : nodes) {
-            vector<int> v;
-            for (auto t : it.second) {
-                for (auto x : t.second) {
-                    v.push_back(x);
-                }
-            }
-            ans.push_back(v);
-        }
-        return ans;
+        ans1.push_back(ans);
+    }
+    return ans1;
     }
 };
