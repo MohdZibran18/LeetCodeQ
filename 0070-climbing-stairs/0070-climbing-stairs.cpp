28cb659a279657805ad198091ca4d413b074795b
Mohd Zibran <2k22.it.32328@gmail.com>
2025-09-06 23:04:15 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-    int ans1=0;
-    vector<int>ans;
-    ans.push_back(0);
-    ans.push_back(1);
-       for(int i=1;i<=n;i++){
-       int first=ans[i-1];
-       int second=ans[i];
-       ans1=(first+second);
-       ans.push_back(ans1);
-       }
-       return ans[ans.size()-1];
+        vector<int>dp(n+1);
+        if(n<=1) return 1;
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]= dp[i-1]+dp[i-2];
+        }
+       return dp[n];
     }
 };
