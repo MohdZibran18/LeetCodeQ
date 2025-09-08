cafd69af7f7767d9ecfcb211708c6bb47e9820c0
Mohd Zibran <2k22.it.32328@gmail.com>
2025-09-08 21:31:49 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -7,9 +7,12 @@ public:
         //base case 
         if(n==1) return nums[0];
         dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        for(int i=2;i<n;i++){
-            int a=nums[i]+dp[i-2];
+        int neg=0;
+        for(int i=1;i<n;i++){
+            int a=nums[i];
+            if(i>1){
+                a+=dp[i-2];
+            }
             int b= dp[i-1];
             dp[i]= max(a,b);
         }
