76478a60b0d989d008df719e1569995fb4a2d7de
Mohd Zibran <2k22.it.32328@gmail.com>
2025-09-12 15:58:36 +0530

    Time: 0 ms (100%), Space: 16.3 MB (41.34%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int solve(vector<vector<int>>& grid,int row , int col, vector<vector<int>>& dp){
+        if(row==0 && col==0 ){
+            return grid[row][col];
+        }
+        if(row<0 || col <0){
+            return INT_MAX;
+        }
+        if(dp[row][col]!=-1){
+            return dp[row][col];
+        }
+        int left = solve(grid,row,col-1,dp); 
+        int up = solve(grid,row-1,col,dp);
+        return dp[row][col] = grid[row][col] + min(left, up);
+    }
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n= grid.size();
+        int m= grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        
+        return solve(grid,n-1,m-1,dp);
+    
+    }
+};
