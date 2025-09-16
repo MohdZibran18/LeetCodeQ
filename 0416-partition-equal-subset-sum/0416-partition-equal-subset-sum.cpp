f076a486d00c7abd5c9aebd86f3b81b4855c1906
Mohd Zibran <2k22.it.32328@gmail.com>
2025-09-16 22:55:55 +0530

    Time: 152 ms (34.49%), Space: 17.3 MB (72.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-bool solve(vector<int>& nums,int sum,int index,vector<vector<int>>&dp){
-        if(sum==0){
-            return true;
-        }if(index==0){
-            return nums[0]==sum;
-        }
-        if(dp[index][sum]!=-1){
-            return dp[index][sum];
-        }
-        bool take = false;
-        bool notTake = false;
-        notTake= solve(nums, sum, index-1,dp);
-        if(nums[index] <= sum){
-            take = solve(nums, sum - nums[index], index-1,dp);
-        }
-        return dp[index][sum]= take || notTake;
-    }
     bool canPartition(vector<int>& nums) {
         int n= nums.size();
         int sum=0;
         for(int i=0;i<n;i++) sum += nums[i];
-
         if(sum%2==1) return false;
-
         sum= sum/2;
-         vector<vector<int>>dp(nums.size(), vector<int>(sum+1,-1));
-        return solve(nums,sum,n-1,dp);
+         vector<vector<bool>>dp(nums.size(), vector<bool>(sum+1,false));
+         // filling base case 
+         for(int i=0;i<n;i++){
+            dp[i][0]= true;
+         }
+         if (nums[0] <= sum) {
+            dp[0][nums[0]] = true;
+        }
+         for(int i=1;i<n;i++){
+            for(int j= 1 ;j <=sum ; j++){
+                bool take = false;
+                bool notTake = false;
+                notTake= dp[i-1][j];
+                if(nums[i] <= j){
+                     take = dp[i-1][j - nums[i]];
+                }
+                 dp[i][j] = take || notTake;
+            }
+         }
+        return dp[n-1][sum];
     }
 };
