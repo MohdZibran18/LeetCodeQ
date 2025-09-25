30c0845aaa11b3f81cf72b0c24edc9e9336245b8
Mohd Zibran <2k22.it.32328@gmail.com>
2025-09-25 11:02:06 +0530

    Time: 0 ms (100%), Space: 13.3 MB (55.55%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& nums,int row,int col,vector<vector<int>>&dp){
-        //base condition 
-        if(row==0 && col ==0){
-            return nums[row][col];
-        }
-       if(row < 0 || col < 0 || col > row) return INT_MAX;
-       if(dp[row][col]!=-1) return dp[row][col];
-        int top = solve(nums, row-1, col,dp);
-        int dia=  solve(nums, row-1, col-1,dp);
-        return dp[row][col]=nums[row][col]+ min(top,dia);
-    }
     int minimumTotal(vector<vector<int>>& nums) {
         int n = nums.size();
-         int m = nums[n-1].size();
-       vector<vector<int>>dp(n,vector<int>(m,-1));
+        int m = nums[n-1].size();
+        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
+        dp[0][0] = nums[0][0];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < nums[row].size(); col++) { 
+                if (row == 0 && col == 0) continue;
+                int top = (col<dp[row-1].size()) ?dp[row-1][col] :INT_MAX;
+                int dia = ( col-1 >= 0) ? dp[row-1][col-1]:INT_MAX;
+                dp[row][col] = nums[row][col] + min(top, dia);
+            }
+        }
         int ans1=INT_MAX;
-       for(int i=m-1;i>=0;i--){
-          int ans = solve(nums,n-1,i,dp);
-            ans1 = min(ans1,ans);
+        for(int i=0;i<m;i++){
+            ans1= min(ans1,dp[n-1][i]);
         }
         return ans1;
     }
